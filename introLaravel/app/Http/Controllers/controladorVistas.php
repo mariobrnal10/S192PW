972c8ddde05f1e1f943f608f68c8606152739271
mariobrnal10 <145825327+mariobrnal10@users.noreply.github.com>
2024-10-15 18:12:54 -0600

    2da parte practica 5

--- introLaravel/app/Http/Controllers/controladorVistas.php
+++ introLaravel/app/Http/Controllers/controladorVistas.php
@@ -20,6 +20,21 @@ class controladorVistas extends Controller
 
     public function procesarCliente(Request $peticion){
         //return 'La informacion del Cliente llego al Controlador';
-        return $peticion->all();
+
+        // devuelve todo lo que contiene la peticion.
+        //return $peticion->all();
+
+        //la ruta donde se hizo la peticion
+        //return $peticion->path();
+
+        //devuelve la url completa
+        //return $peticion->url();
+
+        //
+        return $peticion->ip();
+
     }
+
+
+
 }
--- introLaravel/resources/views/layouts/plantilla1.blade.php
+++ introLaravel/resources/views/layouts/plantilla1.blade.php
@@ -18,11 +18,11 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
 
-            <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{ route('rutaformulario') }}">Registro de Clientes</a>
+            <li class="nav-item active">
+            <a class="nav-link {{ request()->routeIs('rutaformulario')?'text-warning':''}}" aria-current="page" href="{{ route('rutaformulario') }}">Registro de Clientes</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="{{ route('rutaclientes') }}">Consultar Clientes</a>
+              <a class="nav-link {{request()->routeIs('rutaclientes')?'text-warning':''}}" href="{{ route('rutaclientes') }}">Consultar Clientes</a>
             </li>
 
           </ul>
